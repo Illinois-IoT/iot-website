30ff0414920b4c1a6c7130667ea395829ce54a4d
Lily Li <lily.jiayu@gmail.com>
2022-01-06 15:54:33 -0600

    feat: init research papge

--- src/pages/research.js
+++ src/pages/research.js
@@ -5,13 +5,24 @@ import { PageHero } from "../templates/hero/hero"
 
 import about_hero from "../images/about_hero.png"
 import texture from "../images/texture.jpeg"
+import FlipCards from "../templates/flipcards/flipcards";
+import flipcard from "../images/flipcard.jpeg"
 
 const CaesarPage = () => {
+  const cards = [{primary:"3", secondary:"years old", img: flipcard}, 
+    {primary:"100+", secondary:"undergraduate students", img: flipcard}, 
+    {primary:"10",secondary:"graduate/Ph.D. students", img: flipcard}, 
+    {primary:"4",secondary:"current projects", img: flipcard}, 
+    {primary:"18",secondary:"past projects", img: flipcard}]
+
   return (
     <body>
       <title>Professor Caesar</title>
       <Navigation />
-      <PageHero texture_img={texture} hero_img={about_hero} title={"Research"} subtitle={"Learn more about all current and past projects"}/>      
+      <div className="flex-parent" style={{display:"flex", flexDirection:"column", alignItems:"center", width: "100vw", padding:"0vw 10vw"}}>
+        <PageHero texture_img={texture} hero_img={about_hero} title={"Research"} subtitle={"Learn more about all current and past projects"}/>      
+        <FlipCards items={cards}/>
+      </div>
     </body>
   )
 }
---
+++ src/templates/flipcards/flipcards.js
@@ -0,0 +1,51 @@
+/*
+ * flipcard.js
+ * Author: Lily Li
+ */
+
+import React, {useState} from "react";
+import ReactCardFlip from 'react-card-flip';
+import './flipcards.css'
+import {motion} from 'framer-motion'
+
+export default function FlipCards(props){
+    var items = []
+    for(var i in props.items){
+        items.push(
+          <Card primary={props.items[i].primary} secondary={props.items[i].secondary} img={props.items[i].img}/>
+        );  
+    }
+    return (
+        <div className="cards-div">
+            {items}
+        </div>
+    )
+}
+
+function Card(props){
+    let [hover,setHover]= useState(false);
+    const changeHover = (bool) => {
+        console.log(bool);
+        setHover(bool);
+    }
+    return(
+        <motion.div
+        animate={{ scale: [0.9,1] }}
+        transition={{ duration: 0.5 }}
+        >
+        <ReactCardFlip isFlipped={hover} flipDirection="horizontal" flipSpeedFrontToBack={0.5} flipSpeedBackToFront={0.5}>
+          <div id="flip-card" 
+           onMouseEnter={()=>setHover(true)}>
+                <img src={props.img} id="flip-background-img"/>
+                <h1 className="flip-item-text" style={{fontSize:"50px", color:"white"}}>{props.primary}</h1>
+          </div>
+          <div id="flip-card"
+           onMouseLeave={()=>setHover(false)}
+           style={{backgroundColor:"white"}}>
+                <img src={props.img} id="flip-background-img"/>
+                <h5 className="flip-item-text" id="secondary">{props.secondary}</h5>
+          </div>
+        </ReactCardFlip>
+        </motion.div>
+    );
+}
