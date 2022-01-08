03b51bc4dd382d45fc219dad24420824653674f8
Lily Li <lily.jiayu@gmail.com>
2022-01-08 13:35:14 -0600

    feat: local outreach interest form

--- src/templates/forms/forms.js
+++ src/templates/forms/forms.js
@@ -3,7 +3,6 @@ import { useForm, ValidationError } from '@formspree/react';
 import '../../theme.css';
 import './forms.css';
 import { Paragraph } from '../paragraph/Paragraph';
-import { FormButton } from '../buttons/buttons';
 
 function SupportUsForm() {
   const [state, handleSubmit] = useForm("mlezdkda");
@@ -28,6 +27,8 @@ function SupportUsForm() {
                     type="text" 
                     name="name"
                     className='form-input'
+                    placeholder="ex: John Smith"
+                    required
                 />
             </div>
             <div className='form-row'>
@@ -37,6 +38,8 @@ function SupportUsForm() {
                     type="email" 
                     name="email"
                     className='form-input'
+                    placeholder="ex: iot.cs.illinois@gmail.com"
+                    required
                 />
             </div>
             <ValidationError 
@@ -65,4 +68,109 @@ function SupportUsForm() {
   );
 }
 
-export { SupportUsForm };
+function LocalOutreachInterestForm() {
+    const [state, handleSubmit] = useForm("mgedvbvg");
+    if (state.succeeded) {
+        return (
+          <div className='lightblue-div'>
+              <Paragraph heading="URBANA-CHAMPAIGN AREA OUTREACH" subheading="Send us an email to be notified of our future outreach events"/>
+              <div className="supportus-form">
+              <h5>Successfully submitted. Thank you!</h5>
+              </div>
+          </div>
+        );
+    }
+    return (
+      <div className='lightblue-div'>
+          <Paragraph heading="URBANA-CHAMPAIGN AREA OUTREACH" subheading="Send us an email to be notified of our future outreach events"/>
+          <form onSubmit={handleSubmit} className="supportus-form">
+              <div className='form-row'>
+                  <h5 className='form-label'>Name</h5>
+                  <input
+                      id="name"
+                      type="text" 
+                      name="name"
+                      className='form-input'
+                      placeholder="ex: John Smith"
+                      required
+                  />
+              </div>
+              <div className='form-row'>
+                  <h5 className='form-label'>Email</h5>
+                  <input
+                      id="email"
+                      type="email" 
+                      name="email"
+                      className='form-input'
+                      placeholder="ex: iot.cs.illinois@gmail.com"
+                      required
+                  />
+              </div>
+              <ValidationError 
+                  prefix="Email" 
+                  field="email"
+                  errors={state.errors}
+              />
+              <div className='form-row'>
+                  <h5 className='form-label'>(optional) Which county/city do you reside in?</h5>
+                  <input
+                      id="residency"
+                      type="text" 
+                      name="residency"
+                      className='form-input'
+                      placeholder="ex: Urbana-Champaign, Bloomington, Peoria, etc."
+                  />
+              </div>
+              <div className='form-row'>
+                  <h5 className='form-label'>(optional) Which school do you attend?</h5>
+                  <input
+                      id="residency"
+                      type="text" 
+                      name="residency"
+                      className='form-input'
+                      placeholder="ex: Champaign Central High School, Urbana Middle School, etc."
+                  />
+              </div>
+              <div className='form-row'>
+                  <h5 className='form-label'>Areas of Interest?</h5>
+              </div>
+              <div className='form-chekcbox-div'>
+                <div className='form-chekcbox-row'>
+                    <h6>Internet of Things</h6>
+                    <input
+                        id="interest-iot"
+                        type="checkbox" 
+                        name="interest-iot"
+                    />
+                </div>
+                <div className='form-chekcbox-row'>
+                    <h6>Programming</h6>
+                    <input
+                        id="interest-programming"
+                        type="checkbox" 
+                        name="interest-programming"
+                    />
+                </div>
+                <div className='form-chekcbox-row'>
+                    <h6>Electrical hardware (sensors)</h6>
+                    <input
+                        id="interest-hardware"
+                        type="checkbox" 
+                        name="interest-hardware"
+                    />
+                </div>
+              </div>
+              <ValidationError 
+                  prefix="Message" 
+                  field="message"
+                  errors={state.errors}
+              />
+              <button type="submit" disabled={state.submitting} id="basic-button">
+                  Submit
+              </button>
+          </form>
+      </div>
+    );
+  }
+
+export { SupportUsForm, LocalOutreachInterestForm };
