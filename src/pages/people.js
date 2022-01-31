9fa506dd9781f9f180d1f3cbee8edb99fa7da79e
Lily Li <lily.jiayu@gmail.com>
2022-01-30 20:09:00 -0600

    feat: people's page

---
+++ src/pages/people.js
@@ -0,0 +1,58 @@
+import * as React from "react"
+
+import "../theme.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from "../templates/navigation/navigation"
+import { PageHero } from "../templates/hero/hero"
+
+import about_hero from "../images/hero2.jpeg"
+import texture from "../images/texture.jpeg"
+import Footer from "../templates/footer/footer";
+import { Paragraph } from "../templates/paragraph/Paragraph";
+import { PeopleTable } from "../templates/people/people";
+
+import { Helmet } from "react-helmet"
+import favicon from '../images/block-i.png';
+
+import lily_li from "../images/people/lily_li.jpeg"
+import daniel_abdoue from "../images/people/daniel_abdoue.png"
+import milind_sagaram from "../images/people/milind_sagaram.jpeg"
+import rishabh_bassi from "../images/people/rishabh_bassi.jpeg"
+import shrirang_bagdi from "../images/people/shrirang_bagdi.jpeg"
+import max_fan from "../images/people/max_fan.jpeg"
+
+const people_arr = [
+    {name:"Rishabh Bassi", bio:"BS Computer Science, 2019", involvement:"Explain Net, NLP in Network systems", img: rishabh_bassi},
+    {name:"Daniel Abdoue", bio:"BS Computer Engineering, 2022", involvement:"mmWave Research", img: daniel_abdoue},
+    {name:"Lily Li", bio:"BS Computer Science, 2022", involvement:"Website Manager, Illinois of Things Coordinator", img: lily_li},
+    {name:"Shrirang Bagdi", bio:"BS Statistics & Computer Science , 2023", involvement:"ML-algorithms", img: shrirang_bagdi},
+    {name:"Max Fan", bio:"BS CS + Philosophy, 2024", involvement:"IoT security", img:max_fan},
+    {name:"Milind Sagaram", bio:"BS Computer Engineering, 2025", involvement:"Illinois of Things, AI Drone Research", img: milind_sagaram},
+
+]
+
+const ConnectPage = () => {
+  return (
+    <body>
+      <Helmet
+        title="IoT@Illinois - People"
+        meta={[
+            { name: 'description', content: 'The official website for The Illinois Center for Internet of Things.' },
+            { name: 'keywords', content: 'Illinois IoT' },
+        ]}
+        link={[
+            { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+        ]}
+      />
+      <Navigation />
+      <div className="flex-parent" style={{display:"flex", flexDirection:"column", alignItems:"center", width: "100vw", padding:"0vw 10vw"}}>
+        <PageHero texture_img={texture} hero_img={about_hero} title={"PEOPLE"}/>      
+        <Paragraph heading="THE RESEARCHERS" subheading="Meet some of the undergraduate, graduate, and PhD students at IoT@Illinois" />
+        <PeopleTable people={people_arr}/>
+      </div>
+      <Footer />
+    </body>
+  )
+}
+
+export default ConnectPage
---
+++ src/templates/people/people.js
@@ -0,0 +1,33 @@
+/*
+ * people.js
+ * Author: Lily Li
+ */
+
+import React from 'react';
+import '../../theme.css';
+import "./people.css";
+
+function PeopleTable(props) {
+    var people = []
+    for(var p in props.people){
+        people.push(<Person name={props.people[p].name} bio={props.people[p].bio} involvement={props.people[p].involvement} img={props.people[p].img}/>);
+    }
+    return(
+        <div id="people-div">
+            {people}
+        </div>
+    );
+}
+
+function Person(props) {
+    return(
+        <div id="person-div">
+            <img src={props.img} id="person-img"/>
+            <h4 style={{color:"var(--illini-blue)"}}>{props.name}</h4>
+            <p style={{color:"var(--industrial-blue)"}}>{props.bio}</p>
+            <p>{props.involvement}</p>
+        </div>
+    );
+}
+
+export {PeopleTable};
