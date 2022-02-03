60487404eb35ea99818fe713e4df8df7921b300d
Lily Li <lily.jiayu@gmail.com>
2022-02-03 17:56:07 -0600

    feat: lab assistant form

--- src/templates/forms/forms.js
+++ src/templates/forms/forms.js
@@ -314,4 +314,144 @@ function MakerspaceAccessRequestForm() {
     );
 }
 
-export { SupportUsForm, LocalOutreachInterestForm, ContactUsForm, MakerspaceAccessRequestForm };
+function LabAssistantApplicationForm() {
+    const [state, handleSubmit] = useForm("mbjwgapq");
+    if (state.succeeded) {
+        return (
+          <div className='lightblue-div'>
+              <Paragraph heading="LAB ASSISTANT APPLICATION"/>
+              <div className="form-div">
+              <h5>Successfully submitted. Thank you!</h5>
+              </div>
+          </div>
+        );
+    }
+
+    return (
+      <div className='lightblue-div'>
+          <Paragraph heading="LAB ASSISTANT APPLICATION" subheading="Please read this entire page before applying"/>
+          <form onSubmit={handleSubmit} className="form-div">
+              <div className='form-row'>
+                  <h5 className='form-label'>Name</h5>
+                  <input
+                      id="name"
+                      type="text" 
+                      name="name"
+                      className='form-input'
+                      placeholder="ex: John Smith"
+                      required
+                  />
+              </div>
+              <div className='form-row'>
+                  <h5 className='form-label'>Illinois Email (@illinois.edu)</h5>
+                  <input
+                      id="email"
+                      type="email" 
+                      name="email"
+                      className='form-input'
+                      placeholder="ex: js@illinois.edu"
+                      required
+                  />
+              </div>
+              <ValidationError 
+                  prefix="Email" 
+                  field="email"
+                  errors={state.errors}
+              />
+              <div className='form-row'>
+                  <h5 className='form-label'>Current Grade Level</h5>
+                  <select name="subject" id="subject" className='form-input'>
+                    <option value="Undergrad - Freshman">Undergrad - Freshman</option>
+                    <option value="Undergrad - Sophomore">Undergrad - Sophomore</option>
+                    <option value="Undergrad - Junior">Undergrad - Junior</option>
+                    <option value="Undergrad - Senior">Undergrad - Senior</option>
+                    <option value="Grad Student">Grad Student</option>
+                    <option value="PhD Student">PhD Student</option>
+                  </select> 
+              </div>
+              <div className='form-row'>
+                  <h5 className='form-label'>Check all that applies</h5>
+              </div>
+              <div className='form-chekcbox-div'>
+                <div className='form-chekcbox-row'>
+                    <h6>Have general programming experience</h6>
+                    <input
+                        id="qualification-programming"
+                        type="checkbox" 
+                        name="qualification-programming"
+                        value="have general programming experience"
+                        className='form-checkbox'
+                    />
+                </div>
+                <div className='form-chekcbox-row'>
+                    <h6>Have experience in Python</h6>
+                    <input
+                        id="qualification-python"
+                        type="checkbox" 
+                        name="qualification-python"
+                        value="Have experience in Python"
+                        className='form-checkbox'
+                    />
+                </div>
+                <div className='form-chekcbox-row'>
+                    <h6>Knowledgeable in IoT</h6>
+                    <input
+                        id="qualification-knowledge-iot"
+                        type="checkbox" 
+                        name="qualification-knowledge-iot"
+                        value="knowledgeable in IoT"
+                        className='form-checkbox'
+                    />
+                </div>
+                <div className='form-chekcbox-row'>
+                    <h6>Have interest in IoT</h6>
+                    <input
+                        id="qualification-interest-iot"
+                        type="checkbox" 
+                        name="qualification-interest-iot"
+                        value="Have interest in IoT"
+                        className='form-checkbox'
+                    />
+                </div>
+                <div className='form-chekcbox-row'>
+                    <h6>Have taken CS437 (Topics in Internet of Things)</h6>
+                    <input
+                        id="qualification-cs437"
+                        type="checkbox" 
+                        name="qualification-cs437"
+                        value="Have taken CS437 (Topics in Internet of Things)"
+                        className='form-checkbox'
+                    />
+                </div>
+                <div className='form-chekcbox-row'>
+                    <h6>Good at interacting with minors (high school students)</h6>
+                    <input
+                        id="qualification-aptitude-minors"
+                        type="checkbox" 
+                        name="qualification-aptitude-minors"
+                        value="Good at interacting with minors (high school students)"
+                        className='form-checkbox'
+                    />
+                </div>
+              </div>
+              <div className='form-chekcbox-div'>
+                <div className='form-chekcbox-row'>
+                    <h6>I certify I will be on campus during the week of the camp</h6>
+                    <input
+                        id="certify-on-campus"
+                        type="checkbox" 
+                        name="certify-on-campusn"
+                        value="I certify I will be on campus during the week of the camp"
+                        className='form-checkbox'
+                    />
+                </div>
+              </div>
+              <button type="submit" disabled={state.submitting} id="basic-button">
+                  Submit
+              </button>
+          </form>
+      </div>
+    );
+}
+
+export { SupportUsForm, LocalOutreachInterestForm, ContactUsForm, MakerspaceAccessRequestForm, LabAssistantApplicationForm };
