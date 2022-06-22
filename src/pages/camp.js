f39f8a24469b987b73cdd4615ba055ddc88a2ab2
Lily Li <jiayuli6@illinois.edu>
2022-06-22 18:29:09 -0500

    V2.3.12

--- src/pages/camp.js
+++ src/pages/camp.js
@@ -188,43 +188,43 @@ const CampPage = () => {
                     {name: "Project Documentation", href: "/camp/day3"}
                     ]}/>
             </div>
-            <div className="semi-hide-div">
-                <div style={{display:"flex", flexDirection:"column", gap:"20px", padding: "40px 10vw"}}>
-                    <Paragraph heading={"DAY 4"} />
-                    <Collapsible 
-                        header={"Lecture Slides"}
-                        detail={
+            
+            <div style={{display:"flex", flexDirection:"column", gap:"20px", padding: "40px 10vw"}}>
+                <Paragraph heading={"DAY 4"} />
+                <Collapsible 
+                    header={"Lecture Slides"}
+                    detail={
+                    <div>
+                        <ReactGoogleSlides
+                            height="400px"
+                            width="100%"
+                            slidesLink="https://docs.google.com/presentation/d/1Zc_tQ3liWj7LHjZ7qVZ9vsjlFnihcsaj/edit?usp=sharing&ouid=115923064281253178961&rtpof=true&sd=true"
+                            slideDuration={5}
+                            showControls
+                            loop
+                        />
+                    </div>}
+                />
+                <Collapsible 
+                    header={"Mini Lab Slides"}
+                    detail={
                         <div>
-                            <ReactGoogleSlides
-                                height="400px"
-                                width="100%"
-                                slidesLink="https://docs.google.com/presentation/d/1Zc_tQ3liWj7LHjZ7qVZ9vsjlFnihcsaj/edit?usp=sharing&ouid=115923064281253178961&rtpof=true&sd=true"
-                                slideDuration={5}
-                                showControls
-                                loop
-                            />
-                        </div>}
-                    />
-                    <Collapsible 
-                        header={"Mini Lab Slides"}
-                        detail={
-                            <div>
-                            <ReactGoogleSlides
-                                height="400px"
-                                width="100%"
-                                slidesLink="https://docs.google.com/presentation/d/1kjoMQowrxY6plJxlY_2RcjpgJtdzzVomDhv86vIQc3Y/edit?usp=sharing"
-                                slideDuration={5}
-                                showControls
-                                loop
-                            />
-                        </div>}
-                    />
-                    <ButtonContainer 
-                        items={[
-                        {name: "Project Documentation", href: "/camp/day4"}
-                        ]}/>
-                </div>
-
+                        <ReactGoogleSlides
+                            height="400px"
+                            width="100%"
+                            slidesLink="https://docs.google.com/presentation/d/1kjoMQowrxY6plJxlY_2RcjpgJtdzzVomDhv86vIQc3Y/edit?usp=sharing"
+                            slideDuration={5}
+                            showControls
+                            loop
+                        />
+                    </div>}
+                />
+                <ButtonContainer 
+                    items={[
+                    {name: "Project Documentation", href: "/camp/day4"}
+                    ]}/>
+            </div>
+            <div className="semi-hide-div">
                 <div className="lightblue-div" style={{display:"flex", flexDirection:"column", gap:"20px"}}>
                     <Paragraph heading={"DAY 5"} />
                     <Collapsible 
